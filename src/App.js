bddcbdef91fe83dcc5a390fc7c9537222eea949d
ak192410 <aakashsmehta03@gmail.com>
2024-03-30 21:48:31 +0800

    directory updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
-import Navbar from './Components/Navbar/Navbar'
+import Navbar from './Components/Navbar/Navbar';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+import Shop from './Pages/Shop';
+import Cart from './Pages/Cart';
+import Product from './Pages/Product';
+import NewItem from './Pages/NewItem';
+import LoginSignup from './Pages/LoginSignup';
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/Cart' element={<Cart/>}/>
+        <Route path='/Product' element={<Product/>}>
+          <Route path=':productId' element={<Product/>}/>
+        </Route>
+        <Route path='/NewItem' element={<NewItem/>}/>
+        <Route path='/Login' element={<LoginSignup/>}/>
+      </Routes>
     </div>
   );
 }
