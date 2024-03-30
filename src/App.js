84bbc8bed4f359d5b070657c8a8f5a2ebd7f541d
ak192410 <aakashsmehta03@gmail.com>
2024-03-30 22:01:21 +0800

    took out the routing functionality temporarily

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from 'react-router-dom';
 import Shop from './Pages/Shop';
 import Cart from './Pages/Cart';
 import Product from './Pages/Product';
 import NewItem from './Pages/NewItem';
 import LoginSignup from './Pages/LoginSignup';
+{/*
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  UseRoutes
+} from 'react-router-dom';
+*/}
+
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar/> 
+{/*
       <Routes>
         <Route path='/' element={<Shop/>}/>
         <Route path='/Cart' element={<Cart/>}/>
@@ -23,6 +28,7 @@ function App() {
         <Route path='/NewItem' element={<NewItem/>}/>
         <Route path='/Login' element={<LoginSignup/>}/>
       </Routes>
+  */}
     </div>
   );
 }
